946fb690ec11fb698af38f1db9ff32644c5bd8df
SonDang <sondanguit@gmail.com>
2023-11-19 16:12:59 +0700

    Something fix ui header agency

--- src/app/(authenticated)/agency/(components)/(profile)/AgencyImageAndName.tsx
+++ src/app/(authenticated)/agency/(components)/(profile)/AgencyImageAndName.tsx
@@ -1,78 +1,69 @@
-'use client';
+"use client";
 
-import React, { useEffect } from 'react'
-import { Image } from '@nextui-org/react';
-import { Card, CardDescription, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import DialogCustom from '@/components/ui/dialogCustom'
-import { useDoiTac } from '@/hooks/useDoiTac';
-import { useQuery } from '@tanstack/react-query';
-import { Skeleton } from '@nextui-org/react';
-import { ThongTinForm } from './(edit)/ThongTinForm';
-
-
+import React, { useEffect } from "react";
+import { CiEdit } from "react-icons/ci";
+import { Button } from "@/components/ui/button";
+import DialogCustom from "@/components/ui/dialogCustom";
+import { useDoiTac } from "@/hooks/useDoiTac";
+import { useQuery } from "@tanstack/react-query";
+import { ThongTinForm } from "./(edit)/ThongTinForm";
 
 function AgencyImageAndName({ session }) {
-    const [loaiDoiTac, setLoaiDoiTacValue] = React.useState();
-    const [isLoaded, setIsLoaded] = React.useState(false);
-    const [isOpen, setOpen] = React.useState(false);
-    const { fetchDoiTacTheoId } = useDoiTac();
-    const [giayPhepKinhDoanhImageFiles, setGiayPhepKinhDoanhImageFiles] = React.useState([]);
+  const [loaiDoiTac, setLoaiDoiTacValue] = React.useState();
+  const [isOpen, setOpen] = React.useState(false);
+  const { fetchDoiTacTheoId } = useDoiTac();
+  const [giayPhepKinhDoanhImageFiles, setGiayPhepKinhDoanhImageFiles] =
+    React.useState([]);
 
+  const { data: userInfo } = useQuery({
+    queryKey: ["userInfo", session?.user?.id],
+    queryFn: async () => {
+      const res = await fetchDoiTacTheoId(session?.user?.id);
+      return res?.[0];
+    },
+  });
 
-    const { data: userInfo } = useQuery({
-        queryKey: ['userInfo', session?.user?.id],
-        queryFn: async () => {
-            const res = await fetchDoiTacTheoId(session?.user?.id);
-            return res?.[0];
-        },
-    });
+  useEffect(() => {
+    if (userInfo) {
+      if (userInfo?.giayPhepKinhDoanh != undefined) {
+        setGiayPhepKinhDoanhImageFiles(JSON.parse(userInfo!.giayPhepKinhDoanh));
+        console.log(giayPhepKinhDoanhImageFiles);
+        setLoaiDoiTacValue("doanhnghiep");
+      } else {
+        setLoaiDoiTacValue("canhan");
+        console.log("done");
+      }
+    }
+  }, [userInfo]);
 
-    useEffect(() => {
-        if (userInfo) {
-            setIsLoaded(true);
-            if (userInfo?.giayPhepKinhDoanh != undefined) {
-                setGiayPhepKinhDoanhImageFiles(JSON.parse(userInfo!.giayPhepKinhDoanh));
-                console.log(giayPhepKinhDoanhImageFiles);
-                setLoaiDoiTacValue('doanhnghiep');
-            } else {
-                setLoaiDoiTacValue('canhan');
-                console.log("done");
-            }
-        }
-    }, [userInfo]);
+  return (
+    <div className="flex flex-row justify-between">
+      <h1 className="text-xl font-semibold">Hồ sơ đối tác</h1>
+      <Button
+        className="mt-2 md:mt-0 flex flex-row bg-transparent border-1 border-red-400 text-black transition ease-in-out hover:bg-white hover:text-red-400 hover:scale-105 gap-3"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Chỉnh sửa <CiEdit className="w-6 h-6" />
+      </Button>
 
-    return (
+      <DialogCustom
+        className="w-full lg:w-[70%] h-[80%] lg:h-[95%] flex items-center justify-center"
+        isModalOpen={isOpen}
+        setIsModalOpen={setOpen}
+        notShowClose={false}
+      >
         <div>
-            <Skeleton isLoaded={isLoaded} className='rounded-lg mt-4'>
-
-                <Card className="bg-white p-4 rounded-lg shadow-md flex flex-col md:flex-row items-center mt-4">
-                    <div className="flex-shrink-0 w-32 h-32 md:w-32 md:h-32 rounded-full overflow-hidden mb-4 md:mr-4 md:mb-0">
-                        <Image src={userInfo?.avatar} className="w-full h-full object-cover" />
-                    </div>
-                    <div className="flex-grow">
-                        <CardTitle className="text-lg font-semibold">{userInfo?.name}</CardTitle>
-                        <CardDescription>{userInfo?.email}</CardDescription>
-                    </div>
-                    <Button className="mt-2 md:mt-0" onClick={() => {
-                        setOpen(true);
-                    }}>Chỉnh sửa</Button>
-
-                    <DialogCustom className='w-full lg:w-[70%] h-[80%] lg:h-[95%] flex items-center justify-center' isModalOpen={isOpen} setIsModalOpen={setOpen} notShowClose={false}>
-
-                        <div>
-                            <div className="flex flex-col space-y-3">
-                                {loaiDoiTac ? (
-                                    <ThongTinForm
-                                        loaiDoiTac={loaiDoiTac} userInfo={userInfo} />
-                                ) : null}
-                            </div>
-                        </div>
-                    </DialogCustom>
-                </Card>
-            </Skeleton>
+          <div className="flex flex-col space-y-3">
+            {loaiDoiTac ? (
+              <ThongTinForm loaiDoiTac={loaiDoiTac} userInfo={userInfo} />
+            ) : null}
+          </div>
         </div>
-    );
+      </DialogCustom>
+    </div>
+  );
 }
 
 export default AgencyImageAndName;
--- src/app/(authenticated)/agency/(components)/(profile)/AgencyInfo.tsx
+++ src/app/(authenticated)/agency/(components)/(profile)/AgencyInfo.tsx
@@ -1,23 +1,24 @@
-'use client'
+"use client";
 
-import React, { useEffect } from 'react'
-import { Card } from '@/components/ui/card'
-import { useDoiTac } from '@/hooks/useDoiTac';
-import { useQuery } from '@tanstack/react-query';
-import { Skeleton } from '@nextui-org/react';
+import React, { useEffect } from "react";
+import { Card } from "@/components/ui/card";
+import { useDoiTac } from "@/hooks/useDoiTac";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@nextui-org/react";
+import { MdOutlineEmail, MdPermIdentity } from "react-icons/md";
+import { IoIosPhonePortrait } from "react-icons/io";
+import { CiLocationOn } from "react-icons/ci";
 
 function AgencyInfo({ session }) {
-
   const [isLoaded, setIsLoaded] = React.useState(false);
 
   const { data: userInfo } = useQuery({
-    queryKey: ['userInfo', session?.user?.id],
+    queryKey: ["userInfo", session?.user?.id],
     queryFn: async () => {
       const res = await fetchDoiTacTheoId(session?.user?.id);
       return res?.[0];
     },
   });
-
 
   const { fetchDoiTacTheoId } = useDoiTac();
 
@@ -29,30 +30,55 @@ function AgencyInfo({ session }) {
 
   return (
     <div>
-      <Skeleton isLoaded={isLoaded} className='rounded-lg mt-4'>
-        <Card className="bg-white p-4 rounded-lg shadow-md relative mt-4">
-          <h1 className="text-xl font-semibold">Thông tin cá nhân</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-            <div>
-              <h2 className="font-semibold">Họ và tên</h2>
-              <p>{userInfo?.name}</p>
+      <h1 className="text-xl font-medium">Thông tin đối tác</h1>
+      <Skeleton isLoaded={isLoaded} className="rounded-lg">
+        <Card className="bg-white p-6 rounded-lg shadow-md relative mt-4">
+          <div className="flex flex-col gap-6 mt-4">
+            <div className="w-full flex justify-center">
+              <div className="border-2 border-red-400 rounded-full w-[180px] md:w-[270px] h-[180px] md:h-[270px] overflow-hidden">
+                <img
+                  src={userInfo?.avatar}
+                  className="w-full h-full object-cover"
+                  alt={""}
+                />
+              </div>
             </div>
-            <div>
-              <h2 className="font-semibold">Số điện thoại</h2>
-              <p>{userInfo?.phoneNumber}</p>
-            </div>
-            <div>
-              <h2 className="font-semibold">Email</h2>
-              <p>{userInfo?.email}</p>
-            </div>
-            <div>
-              <h2 className="font-semibold">Địa chỉ</h2>
-              <p>{userInfo?.diaChi}</p>
+            <div className="w-full space-y-4">
+              <div>
+                <p className="text-sm text-gray-600">Họ tên</p>
+                <div className="flex flex-row gap-2">
+                  <MdPermIdentity className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  <p className="text-sm text-slate-800">{userInfo?.name}</p>
+                </div>
+              </div>
+              <div>
+                <p className="text-sm text-gray-600">Điện thoại</p>
+                <div className="flex flex-row gap-2">
+                  <IoIosPhonePortrait className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  <p className="text-sm text-slate-800">
+                    {userInfo?.phoneNumber}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <p className="text-sm text-gray-600">Email</p>
+                <div className="flex flex-row gap-2">
+                  <MdOutlineEmail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  <p className="text-sm text-slate-800">{userInfo?.email}</p>
+                </div>
+              </div>
+              <div>
+                <p className="text-sm text-gray-600">Địa chỉ</p>
+                <div className="flex flex-row gap-2">
+                  <CiLocationOn className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  <p className="text-sm text-slate-800">{userInfo?.diaChi}</p>
+                </div>
+              </div>
             </div>
           </div>
         </Card>
       </Skeleton>
-      <Skeleton isLoaded={isLoaded} className='rounded-lg mt-4'>
+      <Skeleton isLoaded={isLoaded} className="rounded-lg mt-4">
         <Card className="bg-white p-4 rounded-lg shadow-md relative mt-4">
           <h1 className="text-xl font-semibold">Số lượt đăng bài đang có</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
@@ -70,10 +96,9 @@ function AgencyInfo({ session }) {
             </div>
           </div>
         </Card>
-      </Skeleton>           
+      </Skeleton>
     </div>
-
-  )
+  );
 }
 
-export default AgencyInfo
+export default AgencyInfo;
