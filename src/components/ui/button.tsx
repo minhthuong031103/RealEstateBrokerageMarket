0f58b1b047fa99454ffef3f6313839ce85972cae
SonDang <sondanguit@gmail.com>
2023-11-19 10:18:52 +0700

    Complete fix ui (public)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-red-400 text-primary-foreground hover:bg-slate-800/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
